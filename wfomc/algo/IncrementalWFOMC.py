642ec2a98c5d091cc20e874075368f17d277e911
SunShixin <70331428+SunShixin6@users.noreply.github.com>
2025-03-14 14:22:58 +0800

    在打印树之前保存

--- wfomc/algo/IncrementalWFOMC.py
+++ wfomc/algo/IncrementalWFOMC.py
@@ -19,7 +19,6 @@ def incremental_wfomc(context: WFOMCContext,
     res = Rational(0, 1)
     domain_size = len(domain)
 
-
     ccs: List[int] = list()
     gen_vars: List[RingElement] = list()
     if context.contain_cardinality_constraint():
@@ -34,7 +33,7 @@ def incremental_wfomc(context: WFOMCContext,
             ccs.append(param)
             gen_vars.append(pred2var[pred])
 
-    def GetCoef(poly, gen_vars):  # 定义一个辅助函数 GetCoef，用于处理多项式和生成变量。
+    def get_coef(poly, gen_vars):  # 定义一个辅助函数 get_coef，用于处理多项式和生成变量。
         for degrees, coeff in coeff_dict(poly, gen_vars):  # 遍历通过 coeff_dict 函数获取的多项式的系数字典，degrees 是多项式的各项的指数，coeff 是系数。
             yield degrees, coeff  # 如果多项式的各项指数小于等于约束条件 ccs 中的相应值，则返回该项的指数和系数。
 
@@ -45,16 +44,17 @@ def incremental_wfomc(context: WFOMCContext,
         n_cells = len(cells)  # 计算单元格的数量
         domain_size = len(domain)
 
-        T = defaultdict(lambda:Rational(0,1))
+        T = defaultdict(lambda: Rational(0, 1))
         for j, cell in enumerate(cells):  # NOTE: line 2
             w_j = expand(cell_graph.get_cell_weight(cell))  # 多项式，要expand一下
-            delta_j = tuple(int(i == j) for i in range(n_cells))  # 构造 δ_j（在 j 位置为1，其余为0的元组）。
-            for degrees, coeff in GetCoef(w_j, gen_vars):  # 提取每一项的指数和系数，degrees是一个元组（对文章算法里面的h），如（0,0）（0,1）（1,1）。1x+y + x*y deg:(0,1) # NOTE: line 3
-                T[(delta_j, degrees)] = coeff  # 这里面w_jh就是约束为css的时候对应的权重 # NOTE line 4
+            k_new = tuple(int(i == j) for i in range(n_cells))  # 构造 δ_j（在 j 位置为1，其余为0的元组）。
+            for degrees, coeff in get_coef(w_j, gen_vars):  # 提取每一项的指数和系数，degrees是一个元组（对文章算法里面的h），如（0,0）（0,1）（1,1）。1x+y + x*y deg:(0,1) # NOTE: line 3
+                T[(k_new, degrees)] = coeff  # 这里面w_jh就是约束为css的时候对应的权重 # NOTE line 4
+
 
         for i in range(2, domain_size + 1):  # domain中剩下的元素 NOTE: line 7
             old_T = T  # 把上一轮的结果作为 T_{i-1}。
-            T = defaultdict(lambda:Rational(0,1))  # 新建一个空字典，用于存储 T_i。
+            T = defaultdict(lambda: Rational(0, 1))  # 新建一个空字典，用于存储 T_i。
 
             for j, cell in enumerate(cells):  # 遍历每个可以放元素的cell type C_j。NOTE: line 8
                 w_j = cell_graph.get_cell_weight(cell)  # 获得一个选定的单元 C_j 的 w_j权重。
@@ -71,9 +71,9 @@ def incremental_wfomc(context: WFOMCContext,
                             cell_graph.get_two_table_weight((cell, cells[k]))
                             ** int(k_old[k]) for k in range(n_cells)
                         ), Rational(1, 1)
-                    )) # 这是选中的cell_j和所有cell的r权重成绩，表示新引入这个元素带来了哪些新权重
+                    ))  # 这是选中的cell_j和所有cell的r权重成绩，表示新引入这个元素带来了哪些新权重
 
-                    for degrees, W_delta in GetCoef(symbolic_weight, gen_vars):  # 系数coeff也就是W_k_old_j_t
+                    for degrees, W_delta in get_coef(symbolic_weight, gen_vars):  # 系数coeff也就是W_k_old_j_t
                         d_new = tuple(d_old + degree for d_old, degree in zip(d_old, degrees))  # 更新d_old
                         if not all(d <= c for d, c in zip(d_new, ccs)):  # d_new不断增加，防止得到的d_new 超过给的ccs
                             continue
@@ -88,7 +88,7 @@ def incremental_wfomc(context: WFOMCContext,
         res += weight * res_sum
 
     # with open('./data.pkl', 'wb') as f:
-    #     pickle.dump(data, f)
+    #     pickle.dump(dp_nodes, f)
     #     print("保存成功")
 
     return res
